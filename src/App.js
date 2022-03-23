3e6223d6b8983905de7ba1d1b2f5316293c0e5a5
hernantak <hernantak@gmail.com>
2022-03-23 15:30:15 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import BasicTweetView from './BasicTweetView';
+import { ApolloProvider, ApolloClient } from '@apollo/client'
+import { InMemoryCache } from "apollo-boost";
+import { createHttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+
+import "./App.css";
+
+const GRAPHQL_URL = 'https://blue-surf-590104.us-east-1.aws.cloud.dgraph.io/graphql';
+
+const App = () => {
+  const [client, setClient] = useState(
+    new ApolloClient({
+      uri: GRAPHQL_URL,
+      cache: new InMemoryCache()
+    })
   );
-}
+  const [clientUpdated, setClientUpdated] = useState(false);
+
+  const createAdminClient = async (token) => {
+    const httpLink = createHttpLink({
+      uri: GRAPHQL_URL,
+      options: {
+        reconnect: true,
+      },
+    });
+  
+    const authLink = setContext((_, { headers }) => {
+      // return the headers to the context so httpLink can read them
+      return {
+        headers: {
+          ...headers,
+          "Authorization": token,
+        },
+      };
+    });
+  
+    const client = new ApolloClient({
+      link: authLink.concat(httpLink),
+      cache: new InMemoryCache()
+    });
+
+    setClientUpdated(true);
+    setClient(client);
+    console.log('updated')
+  }
+
+  return (
+    <ApolloProvider client={client}>
+      <BasicTweetView 
+        authenticated={clientUpdated} 
+        onKeyChange={createAdminClient}
+      />
+    </ApolloProvider>
+  );
+};
 
 export default App;
